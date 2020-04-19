4cff54f4eef0b9507e9e46509339bb71a78c011e
Darcy Knox <darcy@cloudcannon.com>
2020-04-20 00:38:33 +1200

    recursive countUp function

--- CountingItUp.java
+++ CountingItUp.java
@@ -1,22 +1,25 @@
+import java.util.*;
 
 public class CountingItUp{
-    
-    public static void main(String[] args){
-	if(args.length == 2){
-	    long n = Long.parseLong(args[0]);
-	    long k = Long.parseLong(args[1]);
-	    long result = countUp(n, k);
-	    System.out.println("For n = " +n+ " and k = " +k+ ", n choose k = " + result);
-	}else{
-	    System.err.println("Please enter n and k as command line arguments for the calculation.");
+
+
+  public static void main(String[] args){
+	   if(args.length == 2){
+	      long n = Long.parseLong(args[0]);
+        long k = Long.parseLong(args[1]);
+        long result = countUp(n, k);
+        System.out.println("For n = " +n+ " and k = " +k+ ", n choose k = " + result);
+      } else {
+        System.err.println("Please enter n and k as command line arguments for the calculation.");
+      }
     }
-}
 
     public static long countUp(long n, long k){
-        long result = 0; //placeholder
-        
-        //Computation method(s) goes here
-        
+      if (k == 0 || n == k) {
+        return 1;
+      } else {
+        long result = countUp(n - 1, k - 1) + countUp(n - 1, k);
         return result;
+      }
     }
 }
